940cb383789b0d0cbb38e3f663f2142e74fe6650
kshivam1177 <shivam.kumar@xeno.in>
2025-04-11 13:20:18 +0530

    request GPS added

--- lib/models/os_detail.dart
+++ lib/models/os_detail.dart
@@ -1,8 +1,21 @@
 class DeviceInfo {
+  /// possible values : android / ios
   final String platform;
+
+  /// possible values : apple/ samsung / huawei / oppo / vivo / xiaomi / oneplus / realme / google
   final String manufacturer;
+
+  /// possible values
+  /// android : 10 / 11 / 12 / 13 / 14
+  /// ios : 10.3.4 / 11.0 / 12.0 / 13.0 / 14.0
   final String osVersion;
+
+  /// possible values : iPhone 12 / iPhone 13 / iPhone 14 / iPhone 15 / Samsung Galaxy S21 / Samsung Galaxy S22
   final String deviceModel;
+
+  /// possible values:
+  /// android : sdk_int values -> 21,22,23,24,25
+  /// ios : major os version -> 10, 11, 12, 13, 14, 15
   final int sdkVersion;
 
   DeviceInfo({
